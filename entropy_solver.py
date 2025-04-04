stellar entropy calculation amended to continue s production until stellar death, tstar units fixed, pmf calculated

entropy_solver.py
@@ -82,7 +82,8 @@ s_smbhmf=[]
 decoupling_index=0
 
 print("Scale factor:")
-
+timecheck = [0]*c.l
+timecheck[-1] = c.t0
 stop=0
 switch=False
 for i in np.arange(2,c.l+1):
@@ -124,7 +125,7 @@ for i in np.arange(2,c.l+1):
         late.time[-i]=late.time[-i+1]*(late.a[-i]/late.a[-i+1])**(3./2.)
     else:
         late.time[-i]=(np.log(late.a[-i]/late.a[-i+1])+late.hub[-i+1]*late.time[-i+1])/late.hub[-i] #Assume H=a'/a^2
-        
+    timecheck[-i] = timecheck[-i+1]-late.a[-i]*c.h
     #comp=32*sigmat*sigmasb*a[l-i+1]*(t_cmb[l-i+1]**4)*xe*(t_bm[l-i+1]-t_cmb[l-i+1])/(3*hub[l-i+1]*me*(c**2)*(1+xe+xhe)*np.sqrt(8*np.pi*G*eps[l-i+1]/3))
     
     #Temperatures
@@ -195,8 +196,10 @@ bhsteps = 100 #Resolution of stellar mass BH IMF function
 
 starmass = np.logspace(np.log10(0.179), np.log10(31), num=starsteps) #masses for stellar IMF in msol
 progmass = np.logspace(np.log10(25), 100, num=bhsteps) # masses for BH progenitor IMF in msol
+pmf = {i:{mass:0 for mass in starmass} for i in range(z30, c.l-1)} #dict of present-day mass function of stars at time i, in format pmf[i][mass] = number density
+
 mstart=10**(1.398-((2-1.3)/bhsteps)) #start at M=25 Msol, s=8.05e+97 mstart has negligible change, most comes from high mass BHs
-#Stellar mass black hole number density tracking, typically at 8.05E+97
+#Star + stellar mass BH tracking
 for i in np.arange(z30,c.l-1):
     late.eps_str[i]=late.eps_str[i-1]+st.strdot(late.hub[i-1])*c.h*late.a[i-1]*c.c**2
     if (i-z30)%((c.l-z30)//10)==0:
@@ -207,7 +210,10 @@ for i in np.arange(z30,c.l-1):
         m = starmass[j]
         mstep = (starmass[j+1]-starmass[j-1])/(2*m*np.log(10)) #dlogM
         dn, dn_err = normalization*st.imf(m)[0]*mstep, normalization*st.imf(m)[1]*mstep
-        late.s_str[i]+=st.entropyrate(m)*late.a[i]*c.h*dn #(ds(m)/dt)*dt*dn(m)
+        for tm in range(i, endlife(i, m, late.time)): #adds stars of mass m produced in timestep i to pmf until timestep of star death
+            pmf[tm][m] += dn
+    for massstep in pmf[i]: #adds entropy contribution of all stars present at time i
+        late.s_str[i]+=st.entropyrate(massstep)*late.a[i]*c.h*pmf[i][massstep] #(ds(m)/dt)*dt*dn(m)
     for j in range(1,bhsteps-1): #BH progenitor IMF
         m = starmass[j]
         mstep = (starmass[j+1]-starmass[j-1])/(2*m*np.log(10)) #dlogM

stellar_functions.py
@@ -20,8 +20,11 @@ def strdot(h): #Time derivative of stellar mass density, h is Hubble constant in
     sol=c.strdot0*x**2/(1+0.012*(x-1)**3*np.exp(0.041*x**(7./4.))) #Hernquist & Springel
     return sol #d(rho)/dt in (kg/m^3)/s
 
-def imf(m): #Initial mass function, redshift independent, m in msol
-    if m<0.5:
+def imf(m): #Kroupa 2001 initial mass function, redshift independent, m in msol
+    if m<0.08:
+        print("Mass is too small for sustainable fusion")
+        return 0, 0
+    elif m<0.5:
         alpha=-1.35
         x_err=0.5
     elif m<1:
@@ -33,15 +36,28 @@ def imf(m): #Initial mass function, redshift independent, m in msol
     return m**(alpha+1), x_err*m**(alpha+1)*np.log(1/m) #dn/dlogM, error of dn/dlogM
 
 def norm(epsstr): #normalization constant between rho_str(z)*1 million yrs (1 star formation event) and imf/pmf integral
-    sol=1.70939 #Integrated in Mathematica from 0 msol to 300 msol
+    sol=1.56119 #Integrated in Mathematica from 0 msol to 300 msol
     a=epsstr/(sol*c.msol*c.c**2)
     return a #1/volume
 
-#Stellar mass (in Msol) to lifetime
+#Stellar mass (in Msol) to lifetime in years
 def tstar(m):
     #sol=10**(10)*m**(-2.5)
-    sol=10**(10.015-3.461*np.log10(m)+0.8157*(np.log10(m)**2)) #elmegreen 2007 paper, eq2
-    return sol
+    sol=10**(10.015-3.461*np.log10(m)+0.8157*(np.log10(m)**2)) #Reid pg 2732
+    return sol #years
+
+def endlife(current, m, time): #binary search to find index right after star death
+    l, r = current, c.l-1
+    target = time[current]+tstar(m)*c.yr #time of stellar death
+    mid = (l+r)//2
+    while l<r:
+        if time[mid]<target:
+            l=mid+1
+        elif time[mid]>target:
+            r=mid
+        else: #in unlikely scenario where time[mid] exactly equals time of death
+            return mid
+    return l
 
 def luminosity(m): #m in msol
     if 0.179<m<=0.45:
@@ -62,14 +78,14 @@ def luminosity(m): #m in msol
 def temp(m): #m in msol
     if m<=1.5: #only reliable down to m=0.179
         R = (0.438*m**2+0.479*m+0.075)*rsol
-        logT = luminosity(m)*lsol/(4*np.pi*R**2*sigmasb)**(1./4.)
+        logT = luminosity(m)*c.lsol/(4*np.pi*R**2*c.sigmasb)**(1./4.)
     else: #only reliable up to m=31
         logT = -0.170*np.log10(m)**2+0.888*np.log10(m)+3.671
     T = 10**(logT)
     return T #K
 
 def entropyrate(m): #time derivative of entropy for one star of mass m
-    return luminosity(m)*lsol/temp(m) #proportional to ds/dt
+    return luminosity(m)*c.lsol/temp(m) #proportional to ds/dt
 
 """
 s per timestep = number density of stars of mass m*(ds(m)/dt)*dt